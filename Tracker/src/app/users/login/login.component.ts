eae9d590aaee404abda8083eee2bf26710c1b5e5
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-07-07 16:34:32 +0300

    Tyle the project page

--- Tracker/src/app/users/login/login.component.ts
+++ Tracker/src/app/users/login/login.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-  login(username: string, password: string) {
-  
+  submit(userName: string, userPassword: string): void {
+    this.http
+      .post(
+        'https://backendjw.herokuapp.com/api/login/',
+        {
+          username: userName,
+          password: userPassword,
+        },
+        { withCredentials: true }
+      )
+      .subscribe((res)=>{
+        console.log(res)
+      })
+
   }
 }
-
 
